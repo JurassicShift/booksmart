3dd5c8764756f49c6871672c7a49d0a490e8042b
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2023-09-03 13:29:37 +0100

    form names

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -6,13 +6,18 @@ const Login = ({ active }) => {
 	const [login, setLogin] = useState(true);
 	const title = login ? 'SignUp' : 'Login';
 
+	const formSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target);
+	}
+
 	return (
 		<div
 			className={`container login__container ${
 				active ? 'active' : 'notActive'
 			}`}
 		>
-			<form className="mt-1 mb-1">
+			<form className="mt-1 mb-1" onSubmit={formSubmit}>
 				<div className="login__title">
 					<h1 className="login__title-h1">Login</h1>
 				</div>
@@ -20,19 +25,19 @@ const Login = ({ active }) => {
 					<label htmlFor="loginUsername" className="form-label text-dark">
 						Username
 					</label>
-					<input type="text" className="form-control" id="loginUsername"></input>
+					<input type="text" name='username' className="form-control" id="loginUsername"></input>
 				</div>
 				<div className="mb-3">
 					<label htmlFor="loginPassword" className="form-label text-dark">
 						Password
 					</label>
-					<input type="password" className="form-control" id="loginPassword"></input>
+					<input type="password" name='user_password' className="form-control" id="loginPassword"></input>
 				</div>
 				<div className="login__btn">
 					<Btn btnTitle="Login" style={{marginRight: "0px"}}/>
 				</div>
 			</form>
-			<form className="mt-1">
+			<form className="mt-1" onSubmit={formSubmit}>
 				<div className="login__title">
 					<h1 className="login__title-h1">SignUp</h1>
 				</div>
@@ -40,19 +45,19 @@ const Login = ({ active }) => {
 					<label htmlFor="username" className="form-label text-dark">
 						Username
 					</label>
-					<input type="text" className="form-control" id="username"></input>
+					<input type="text" name='username' className="form-control" id="username"></input>
 				</div>
 				<div className="mb-3">
 					<label htmlFor="email" className="form-label text-dark">
 						Email address
 					</label>
-					<input type="text" className="form-control" id="email"></input>
+					<input type="text" name='email'className="form-control" id="email"></input>
 				</div>
 				<div className="mb-3">
 					<label htmlFor="password" className="form-label text-dark">
 						Password
 					</label>
-					<input type="password" className="form-control" id="password"></input>
+					<input type="password" name='user_password' className="form-control" id="password"></input>
 				</div>
 				<div className="mb-3">
 					<label htmlFor="confirmPassword" className="form-label text-dark">
@@ -60,6 +65,7 @@ const Login = ({ active }) => {
 					</label>
 					<input
 						type="password"
+						name='user_confirm'
 						className="form-control"
 						id="confirmPassword"
 					></input>
